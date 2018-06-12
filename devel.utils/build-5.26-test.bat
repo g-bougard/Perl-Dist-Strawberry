b8093f78baf1093cf5affdb9f116bdcd943a1b9e
Guillaume Bougard <gbougard@teclib.com>
2018-06-12 14:08:47 +0200

    Fix Wix Tool Set path

--- devel.utils/build-5.26-test.bat
+++ devel.utils/build-5.26-test.bat
@@ -5,9 +5,9 @@ set PERL_USE_UNSAFE_INC=1
 
 set SKIP_MSI_STEP=
 set SKIP_PDL_STEP=
-perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.26.2.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir="C:\Program Fils\WiX Toolset v3.11\bin"
-perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.26.2.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir="C:\Program Fils\WiX Toolset v3.11\bin"
+perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.26.2.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir="C:\Program Files\WiX Toolset v3.11\bin"
+perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.26.2.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir="C:\Program Files\WiX Toolset v3.11\bin"
 
 set SKIP_MSI_STEP=1
 set SKIP_PDL_STEP=1
-perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.26.2.1.pp -notest_core -beta=0 -noperl_64bitint -app_simplename=strawberry-perl-no64 -nointeractive -norestorepoints -wixbin_dir="C:\Program Fils\WiX Toolset v3.11\bin"
+perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.26.2.1.pp -notest_core -beta=0 -noperl_64bitint -app_simplename=strawberry-perl-no64 -nointeractive -norestorepoints -wixbin_dir="C:\Program Files\WiX Toolset v3.11\bin"
