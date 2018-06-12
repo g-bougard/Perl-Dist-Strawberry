ad5d4100f69988f0a64206d21e5a90a4bb55295b
Guillaume Bougard <gbougard@teclib.com>
2018-06-12 17:49:03 +0200

    Try only one build Try without testing modules set 64bitint

--- devel.utils/build-5.26-test.bat
+++ devel.utils/build-5.26-test.bat
@@ -5,5 +5,5 @@ set PERL_USE_UNSAFE_INC=1
 
 set SKIP_MSI_STEP=
 set SKIP_PDL_STEP=
-perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.26.2.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir="C:\Program Files (x86)\WiX Toolset v3.11\bin" -cpan_url="http://cpan.metacpan.org/"
-perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.26.2.1.pp -notest_core -beta=0 -nointeractive -norestorepoints -wixbin_dir="C:\Program Files (x86)\WiX Toolset v3.11\bin" -cpan_url="http://cpan.metacpan.org/"
+rem perl -Mblib ..\script\perldist_strawberry -job ..\share\32bit-5.26.2.1.pp -notest_core -notest_modules -beta=0 -nointeractive -norestorepoints -wixbin_dir="C:\Program Files (x86)\WiX Toolset v3.11\bin" -cpan_url="http://cpan.metacpan.org/"
+perl -Mblib ..\script\perldist_strawberry -job ..\share\64bit-5.26.2.1.pp -notest_core -notest_modules -perl_64bitint -beta=0 -nointeractive -norestorepoints -wixbin_dir="C:\Program Files (x86)\WiX Toolset v3.11\bin" -cpan_url="http://cpan.metacpan.org/"
